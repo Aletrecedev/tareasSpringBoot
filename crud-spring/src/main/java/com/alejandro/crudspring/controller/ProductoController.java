9ccb54500937905e4b5f31fcb9f42e22c0b36b9d
Ale <trececerodev@gmail.com>
2026-02-08 16:55:35 +0100

    Creación del Controlador MVC y rutas

---
+++ crud-spring/src/main/java/com/alejandro/crudspring/controller/ProductoController.java
@@ -0,0 +1,52 @@
+package com.alejandro.crudspring.controller;
+
+import com.alejandro.crudspring.entity.Producto;
+import com.alejandro.crudspring.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+
+    @GetMapping("/productos")
+    public String listarProductos(Model model) {
+        // "productos" es la variable que usaremos en el HTML
+        model.addAttribute("productos", productoService.listarProductos());
+        return "lista_productos";
+    }
+
+
+    @GetMapping("/productos/nuevo")
+    public String mostrarFormularioDeRegistrar(Model model) {
+        Producto producto = new Producto();
+        model.addAttribute("producto", producto);
+        return "crear_producto";
+    }
+
+
+    @PostMapping("/productos")
+    public String guardarProducto(@ModelAttribute("producto") Producto producto) {
+        productoService.guardarProducto(producto);
+        return "redirect:/productos";
+    }
+
+
+    @GetMapping("/productos/editar/{id}")
+    public String mostrarFormularioDeEditar(@PathVariable Long id, Model model) {
+        model.addAttribute("producto", productoService.obtenerProductoPorId(id));
+        return "editar_producto";
+    }
+
+
+    @GetMapping("/productos/eliminar/{id}")
+    public String eliminarProducto(@PathVariable Long id) {
+        productoService.eliminarProducto(id);
+        return "redirect:/productos";
+    }
+}
